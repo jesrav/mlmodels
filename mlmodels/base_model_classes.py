f58710e57e33c5bdfaa197779930152e40618f14
jerav <jesravnbol@hotmail.com>
2019-12-21 13:39:20 +0100

    first commit

---
+++ mlmodels/base_model_classes.py
@@ -0,0 +1,122 @@
+import pickle as pickle
+from datetime import datetime
+from abc import ABCMeta, abstractmethod
+
+
+class BaseModel(metaclass=ABCMeta):
+    """
+    Base class for models
+
+    The class has a save and load method for serializing model objects.
+    It enforces implementation of a fit and predict method and a model name attribute.
+    """
+    def __init__(self):
+        self.model_initiated_dt = datetime.utcnow()
+
+    @property
+    @classmethod
+    @abstractmethod
+    def MODEL_NAME(self):
+        pass
+
+    @abstractmethod
+    def fit(self):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
+
+    def __str__(self):
+        return f'Model: {self.MODEL_NAME},  initiated at {self.model_initiated_dt}'
+
+    def save(self, **kwargs):
+        """Serialize model to file or variable
+        """
+        serialize_dict = self.__dict__
+
+        if "fname" in kwargs.keys():
+            fname = kwargs["fname"]
+            with open(fname, "wb") as f:
+                pickle.dump(serialize_dict, f)
+        else:
+            pickled = pickle.dumps(serialize_dict)
+            return pickled
+
+    def load(self, serialized):
+        """Deserialize model from file or variable"""
+        assert isinstance(serialized, str) or isinstance(
+            serialized, bytes
+        ), "serialized must be a string (filepath) or a bytes object with the serialized model"
+        model = self.__class__()
+
+        if isinstance(serialized, str):
+            with open(serialized, "rb") as f:
+                serialize_dict = pickle.load(f)
+        else:
+            serialize_dict = pickle.loads(serialized)
+
+        # Set attributes of model
+        model.__dict__ = serialize_dict
+
+        return model
+
+
+class BaseTransformer(metaclass=ABCMeta):
+    """
+    Base class for Transformera
+
+    The class has a save and load method for serializing model objects.
+    It enforces implementation of a fit and transform method and a model name attribute.
+    """
+    def __init__(self):
+        self.transformer_initiated_dt = datetime.utcnow()
+
+    @property
+    @classmethod
+    @abstractmethod
+    def TRANSFORMER_NAME(self):
+        pass
+
+    @abstractmethod
+    def fit(self):
+        pass
+
+    @abstractmethod
+    def transform(self):
+        pass
+
+    def __str__(self):
+        return f'Transformer: {self.TRANSFORMER_NAME},  initiated at {self.transformer_initiated_dt}'
+
+    def save(self, **kwargs):
+        """Serialize model to file or variable
+        """
+        serialize_dict = self.__dict__
+
+        if "fname" in kwargs.keys():
+            fname = kwargs["fname"]
+            with open(fname, "wb") as f:
+                pickle.dump(serialize_dict, f)
+        else:
+            pickled = pickle.dumps(serialize_dict)
+            return pickled
+
+    def load(self, serialized):
+        """Deserialize transformer from file or variable
+        """
+        assert isinstance(serialized, str) or isinstance(
+            serialized, bytes
+        ), "serialized must be a string (filepath) or a bytes object with the serialized model"
+        model = self.__class__()
+
+        if isinstance(serialized, str):
+            with open(serialized, "rb") as f:
+                serialize_dict = pickle.load(f)
+        else:
+            serialize_dict = pickle.loads(serialized)
+
+        # Set attributes of model
+        model.__dict__ = serialize_dict
+
+        return model
