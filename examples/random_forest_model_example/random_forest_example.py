a115b551f03c23fe3f42a82ff56d7128705f2d39
jerav <jesravnbol@hotmail.com>
2019-12-22 13:43:57 +0100

    logging model

---
+++ examples/random_forest_model_example/random_forest_example.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import os
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from mlmodels import MLFlowWrapper
+from model_class import RandomForestRegressorModel
+import mlflow
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+if __name__ == '__main__':
+
+    file_path = os.path.dirname(os.path.abspath(__file__))
+
+    model_path = 'model_mlflow'
+    code_path = 'model_class.py'
+    conda_env_path = 'conda.yaml'
+
+    load_dotenv(find_dotenv())
+
+#    mlflow.set_tracking_uri(
+#        'http://{user}:{psw}@ec2-3-124-68-213.eu-central-1.compute.amazonaws.com'.format(
+#            user=os.environ.get("MLFLOW_USER"),
+#            psw=os.environ.get("MLFLOW_PASSWORD"),
+#        )
+#    )
+
+    mlflow.set_experiment('test')
+
+    # Read the wine-quality csv file from the URL
+    csv_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+    data = pd.read_csv(csv_url, sep=';')
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train["quality"]
+    test_y = test["quality"]
+
+    with mlflow.start_run():
+        # Fit model, make predictions and evaluate
+        model = RandomForestRegressorModel(
+            features=train_x.columns,
+            random_forest_params={'n_estimators': 100, 'max_depth': 15},
+        )
+        model.fit(train_x, train_y)
+
+        predicted_qualities = model.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("model_params", model.random_forest_params)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        model_mlflow = MLFlowWrapper(model)
+
+        # log model
+        mlflow.pyfunc.log_model(model_path, code_path=[code_path], python_model=model_mlflow)
