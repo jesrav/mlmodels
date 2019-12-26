50b4c8bde24c03eb68a7a39ad3f02eeda1577739
Ravnbøl Jes <jerav@DFDS.COM>
2019-12-26 14:17:16 +0100

    wip

--- examples/random_forest_model_example/model_class.py
+++ examples/random_forest_model_example/model_class.py
@@ -1,5 +1,7 @@
 from mlmodels import BaseModel
 from sklearn.ensemble import RandomForestRegressor
+from marshmallow_dataframe import RecordsDataFrameSchema
+import json
 
 class RandomForestRegressorModel(BaseModel):
     MODEL_NAME = 'Random forest model'
@@ -18,12 +20,24 @@ class RandomForestRegressorModel(BaseModel):
 
     def fit(self, X, y):
         if self.input_dtypes is None:
-            self.input_dtypes = X[self.features].dtypes.to_dict()
+            self.input_dtypes = X[self.features].dtypes
         self.model.fit(X[self.features], y)
         return self.model
 
     def predict(self, X):
         assert all(list(X.columns) == self.features), f'The following features must be in X: {self.features}'
-        assert X[self.features].dtypes.to_dict() == self.input_dtypes, f'Dtypes must be: {self.input_dtypes}'
+        assert X[self.features].dtypes.to_dict() == self.input_dtypes.to_dict(), f'Dtypes must be: {self.input_dtypes.to_dict()}'
         predictions = self.model.predict(X[self.features])
         return predictions
+
+    def get_model_input_schema(self):
+        class ModelInputSchema(RecordsDataFrameSchema):
+            """Automatically generated schema for model input dataframe"""
+            class Meta:
+                dtypes = self.input_dtypes
+        return ModelInputSchema()
+
+    def json_to_model_input(self, json_data):
+        json_data = json.loads(json_data)
+        model_input_schema = self.get_model_input_schema()
+        return model_input_schema.load(json_data)
---
+++ examples/random_forest_model_example/schema_example.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import os
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from mlmodels import MLFlowWrapper
+from model_class import RandomForestRegressorModel
+import mlflow
+import json
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+if __name__ == '__main__':
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    code_path = str(dir_path / Path('model_class.py'))
+    model_path = str(dir_path / Path('model'))
+    conda_env_path = str(dir_path / Path('conda.yaml'))
+
+    load_dotenv(find_dotenv())
+
+    # Read the wine-quality csv file from the URL
+    csv_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+    data = pd.read_csv(csv_url, sep=';')
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train["quality"]
+    test_y = test["quality"]
+
+    # Fit model, make predictions and evaluate
+    model = RandomForestRegressorModel(
+        features=train_x.columns,
+        random_forest_params={'n_estimators': 100, 'max_depth': 15},
+    )
+    model.fit(train_x, train_y)
+
+    predicted_qualities = model.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    schema = model.get_model_input_schema()
+    data = test_x.sample(5).to_dict(orient='records')
+    data = {'data': data}
+    print(schema.load(data))
+
+    json_data = json.dumps(data)
+    # print(json_data)
+    print(model.predict(model.json_to_model_input(json_data)))
