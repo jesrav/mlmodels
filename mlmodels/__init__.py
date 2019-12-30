d953959c8228afbdbde22d673289d2d58f9cacbd
jerav <jesravnbol@hotmail.com>
2019-12-30 13:34:02 +0100

    Updated init

--- mlmodels/__init__.py
+++ mlmodels/__init__.py
@@ -6,4 +6,4 @@ from .base_model_classes import (
     infer_dataframe_dtypes_from_fit,
     infer_dataframe_features_from_fit,
 )
-from .openapi_yaml_template import open_api_yaml_specification, open_api_dict_specification
+from .model_class_helpers import FeatureSplitModel
