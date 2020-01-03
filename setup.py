22fab97432d3ecfb1706fde37097440fc09bf86b
Ravnbøl Jes <jerav@DFDS.COM>
2020-01-03 10:51:37 +0100

    Bumping version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mlmodels',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Base model and transformer classes.',
     author='Jes Ravnbøl',
     license='',
